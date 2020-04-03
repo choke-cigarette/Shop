218b9223187eb3dec59ad2bfee44760bfe54413e
Viden Ivančev <45499963+choke-cigarette@users.noreply.github.com>
2020-04-03 18:10:06 +0200

    Zapoceo induvidualni projekat

---
+++ artikal.h
@@ -0,0 +1,15 @@
+#ifndef ARTIKAL_H_INCLUDED
+#define ARTIKAL_H_INCLUDED
+
+class Artikal{
+    private:
+        float cena, ocena;
+        char *model, *nazivArtikla;
+        short akcija;
+        bool naStanju, besplatnaDostava;
+        FILE *komentari;
+    public:
+
+};
+
+#endif // ARTIKAL_H_INCLUDED
