e9b9d0978e9d4be4ee38291f50c3dc7363821fed
Viden Ivančev <45499963+choke-cigarette@users.noreply.github.com>
2020-04-03 19:26:33 +0200

    Add files via upload

--- artikal.h
+++ artikal.h
@@ -9,7 +9,62 @@ class Artikal{
         bool naStanju, besplatnaDostava;
         FILE *komentari;
     public:
-
+        float getCena();
+        float getOcena(); //  postavice je neki izmisljeni kupci
+        char* getModel();
+        char* getNazivArtikla();
+        short getAkcija();
+        void ispisiKomentare(); //  nece postojati komentari jer nema user interface-a
+        bool imaLiNaStanju();
+        bool imaLiBesplatnuDostavu();
+        void setCena(float);
+        void setAkcija(short);
+        void setNaStanju(bool);
+        void setBesplatnaDostava(bool);
 };
+
+float Artikal::getCena(){
+    return cena;
+}
+
+float Artikal::getOcena(){
+    return ocena;
+}
+
+char* Artikal::getModel(){
+    return model;
+}
+
+char* Artikal::getNazivArtikla(){
+    return nazivArtikla;
+}
+
+short Artikal::getAkcija(){
+    return akcija;
+}
+
+bool Artikal::imaLiNaStanju(){
+    return naStanju;
+}
+
+bool Artikal::imaLiBesplatnuDostavu(){
+    return besplatnaDostava;
+}
+
+void Artikal::setCena(float c){
+    cena = c;
+}
+
+void Artikal::setAkcija(short a){
+    akcija = a;
+}
+
+void Artikal::setNaStanju(bool s){
+    naStanju = s;
+}
+
+void Artikal::setBesplatnaDostava(bool d){
+    besplatnaDostava = d;
+}
 
 #endif // ARTIKAL_H_INCLUDED
