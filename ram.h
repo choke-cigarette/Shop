23cd8a8e9d4c07f516946151817ab01186c2f576
Viden Ivančev <45499963+choke-cigarette@users.noreply.github.com>
2020-04-23 16:30:26 +0200

    dodat ram

---
+++ ram.h
@@ -0,0 +1,97 @@
+#ifndef RAM_H_INCLUDED
+#define RAM_H_INCLUDED
+using namespace std;
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include "artikal.h"
+
+enum TipRAMa{_DDR1, _DDR2, _DDR3, _DDR4};
+
+class RAM : Artikal{
+    private:
+        TipRAMa memorija;
+        short kapacitet, maksimalnaFrekvencija, latencija;
+        float voltaza;
+    public:
+        RAM();
+        RAM(TipRAMa, short, short, short, float, float, char*, char*, short, bool, bool, FILE *);
+        RAM(RAM &);
+        TipRAMa getMemorija()const;
+        short getKapacitet()const;
+        short getFrekvencija()const;
+        short getLatencija()const;
+        float getVoltaza()const;
+        void setMemorija(TipRAMa);
+        void setKapacitet(short);
+        void setFrekvencija(short);
+        void setLatencija(short);
+        void setVoltaza(float);
+};
+
+RAM::RAM() : Artikal(){
+    memorija = _DDR3;
+    kapacitet = 0;
+    maksimalnaFrekvencija = 0;
+    latencija = 0;
+    voltaza = 0;
+}
+
+RAM::RAM(TipRAMa m, short k, short fr, short l, float v, float c, char *ma, char *na, short a, bool sa, bool da, FILE *f) : Artikal(c, ma, na, a, sa, da, f){
+    memorija = m;
+    kapacitet = k;
+    maksimalnaFrekvencija = fr;
+    latencija = l;
+    voltaza = v;
+}
+
+RAM::RAM(RAM &r) : Artikal(r.getCena(), r.getProizvodjac(), r.getNazivArtikla(), r.getAkcija(), r.imaLiNaStanju(), r.imaLiBesplatnuDostavu(), r.getSlika()->getFajl()){
+    memorija = r.memorija;
+    kapacitet = r.kapacitet;
+    maksimalnaFrekvencija = r.maksimalnaFrekvencija;
+    latencija = r.latencija;
+    voltaza = r.voltaza;
+}
+
+TipRAMa RAM::getMemorija()const{
+    return memorija;
+}
+
+short RAM::getKapacitet()const{
+    return kapacitet;
+}
+
+short RAM::getFrekvencija()const{
+    return maksimalnaFrekvencija;
+}
+
+short RAM::getLatencija()const{
+    return latencija;
+}
+
+float RAM::getVoltaza()const{
+    return voltaza;
+}
+
+void RAM::setMemorija(TipRAMa m){
+    memorija = m;
+}
+
+void RAM::setKapacitet(short k){
+    kapacitet = k;
+}
+
+void RAM::setFrekvencija(short fr){
+    maksimalnaFrekvencija = fr;
+}
+
+void RAM::setLatencija(short l){
+    latencija = l;
+}
+
+void RAM::setVoltaza(float v){
+    voltaza = v;
+}
+
+
+#endif // RAM_H_INCLUDED
